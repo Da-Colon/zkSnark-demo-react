f790660b96188ab5380635e89c9da6be1b265073
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-08-02 10:17:57 -0400

    front-end cleanup

--- src/theme/styles/index.ts
+++ src/theme/styles/index.ts
@@ -5,9 +5,9 @@ import toastStyles from './toast'
 export default {
   global: {
     body: {
-      color: 'cetacean-blue.500',
+      color: 'black.500',
       height: '100%',
-      background: 'baby-powder.500'
+      background: 'gray.200'
     },
     html: {
       scrollBehavior: 'smooth',
@@ -15,55 +15,5 @@ export default {
     },
     ...scrollStyles,
     ...toastStyles,
-    "@font-face": [
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-Light.ttf') format('truetype')`,
-        fontWeight: 300,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-Regular.ttf') format('truetype')`,
-        fontWeight: 400,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-Medium.ttf') format('truetype')`,
-        fontWeight: 500,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-SemiBold.ttf') format('truetype')`,
-        fontWeight: 600,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-Bold.ttf') format('truetype')`,
-        fontWeight: 700,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-ExtraBold.ttf') format('truetype')`,
-        fontWeight: 800,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('/fonts/Figtree-Black.ttf') format('truetype')`,
-        fontWeight: 900,
-        fontStyle: "normal"
-      },
-      {
-        fontFamily: "FigTree",
-        src: `url('fonts/Figtree-Italic.ttf') format('truetype')`,
-        fontWeight: "normal",
-        fontStyle: "italic"
-      }
-    ]
   },
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
 import styles from "./styles";
-import components from "./components";
 
 export const theme = extendTheme({
   fonts: {
     body: "FigTree, system-ui, sans-serif",
   },
   styles,
-  components,
 })
